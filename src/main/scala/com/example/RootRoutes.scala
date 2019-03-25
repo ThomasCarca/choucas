cb6a12e3c0a57f9b541142cfed0b4d12681e48d3
Thomas <boulbestho@eisti.eu>
2019-03-25 15:17:10 +0100

    Created minimal starting project with Akka HTTP

---
+++ src/main/scala/com/example/RootRoutes.scala
@@ -0,0 +1,25 @@
+package com.example
+
+import akka.actor.ActorSystem
+import akka.event.Logging
+
+import scala.concurrent.duration._
+import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.directives.MethodDirectives.get
+import akka.http.scaladsl.server.directives.RouteDirectives.complete
+
+import akka.util.Timeout
+
+trait RootRoutes {
+
+  implicit def system: ActorSystem
+
+  lazy val log = Logging(system, classOf[RootRoutes])
+
+  implicit lazy val timeout = Timeout(5.seconds) // usually we'd obtain the timeout from the system's configuration
+
+  lazy val rootRoutes: Route =
+    get {
+      complete("Hello world !")
+    }
+}
