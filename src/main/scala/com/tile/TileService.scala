15032cb4335a2090ae9e52250f60d6b44977a2a5
Marianne Rentchler <rentchlerm@eisti.eu>
2019-04-08 15:44:30 +0200

    Correction pour bien récupérer les fichiers dans res

--- src/main/scala/com/tile/TileService.scala
+++ src/main/scala/com/tile/TileService.scala
@@ -9,27 +9,27 @@ import scala.sys.process.Process
 object TileService extends DefaultJsonProtocol {
   def TileImage(imageName: String): String = {
     val file = getImageNameInDirectory(imageName)
-    val doGdalPython = Process("gdal2tiles.py ".concat(file + " " + imageName))
-    "Image tiled in /res/".concat(imageName)
+    file match {
+      case "" => "Image doesn't exists"
+      case _ => {
+        val doGdalPython = Process("gdal2tiles.py ".concat(file + " " + imageName))
+        "Image tiled in /res/".concat(imageName)}
+    }
+
   }
 
-  def getImageNameInDirectory(imageName: String): Option[String] = {
-    val d = new File("/res")
-    val fileName: Option[String] = if (d.exists && d.isDirectory) {
+  def getImageNameInDirectory(imageName: String): String = {
+    val d = new File("res")
+    val fileName: String = if (d.exists && d.isDirectory) {
       d.listFiles.filter(_.isFile).toList.filter { file =>
         file.getName.startsWith(imageName)
       } match {
-        case Nil => None
-        case list => Some(list.head.getName)
+        case Nil => ""
+        case list => list.head.getName
       } // get first
     } else {
-      None
+      ""
     }
     fileName
-  }
-
-  def doTile(file: File, dirName: String): String = {
-    //TO DO : lancer programme gdal2tiles.py file dirName
-    "image tuilée et disponible dans /res/dirName"
   }
 }
