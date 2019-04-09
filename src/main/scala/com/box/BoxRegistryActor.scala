daf85a2fcd2021ab987955e130712a03c42e6d2a
Thomas <boulbestho@eisti.eu>
2019-04-09 11:35:18 +0200

    Modified box service for it to send back application/json as Content-Type

--- src/main/scala/com/box/BoxRegistryActor.scala
+++ src/main/scala/com/box/BoxRegistryActor.scala
@@ -1,18 +1,17 @@
 package com.box
 
 import akka.actor.{ Actor, ActorLogging, Props }
-import spray.json._
 
 object BoxRegistryActor {
   final case class ToBoundingBox(coordinates: Vector[Coordinate])
   def props: Props = Props[BoxRegistryActor]
 }
 
-class BoxRegistryActor extends Actor with ActorLogging with BoxJsonSupport {
+class BoxRegistryActor extends Actor with ActorLogging {
   import BoxRegistryActor._
 
   def receive: Receive = {
     case ToBoundingBox(coordinates) =>
-      sender() ! BoxService.createBoundingBox(coordinates).toJson.prettyPrint
+      sender() ! BoxService.createBoundingBox(coordinates)
   }
 }
