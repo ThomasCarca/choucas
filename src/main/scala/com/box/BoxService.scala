481b322dbd8e1da46ccd8256afa2015a09823f11
Thomas <boulbestho@eisti.eu>
2019-04-09 17:39:36 +0200

    Fixed typo

--- src/main/scala/com/box/BoxService.scala
+++ src/main/scala/com/box/BoxService.scala
@@ -6,20 +6,20 @@ object BoxService {
 
   def createBoundingBox(coordinates: Vector[Coordinates]): BoundingBox = {
     coordinates.foldLeft(BoundingBox(Coordinates(90, 180), Coordinates(-90, -180)))(
-      (box, coordinate) => {
-        val swCoordinate = coordinate match {
-          case Coordinates(lat, lon) if lat < box.swCoordinate.lat && lon < box.swCoordinate.lon => Coordinates(lat, lon)
-          case Coordinates(lat, _) if lat < box.swCoordinate.lat => Coordinates(lat, box.swCoordinate.lon)
-          case Coordinates(_, lon) if lon < box.swCoordinate.lon => Coordinates(box.swCoordinate.lat, lon)
-          case _ => box.swCoordinate
+      (box, coordinates) => {
+        val swCoordinates = coordinates match {
+          case Coordinates(lat, lon) if lat < box.swCoordinates.lat && lon < box.swCoordinates.lon => Coordinates(lat, lon)
+          case Coordinates(lat, _) if lat < box.swCoordinates.lat => Coordinates(lat, box.swCoordinates.lon)
+          case Coordinates(_, lon) if lon < box.swCoordinates.lon => Coordinates(box.swCoordinates.lat, lon)
+          case _ => box.swCoordinates
         }
-        val neCoordinate = coordinate match {
-          case Coordinates(lat, lon) if lat > box.neCoordinate.lat && lon > box.neCoordinate.lon => Coordinates(lat, lon)
-          case Coordinates(lat, _) if lat > box.neCoordinate.lat => Coordinates(lat, box.neCoordinate.lon)
-          case Coordinates(_, lon) if lon > box.neCoordinate.lon => Coordinates(box.neCoordinate.lat, lon)
-          case _ => box.neCoordinate
+        val neCoordinates = coordinates match {
+          case Coordinates(lat, lon) if lat > box.neCoordinates.lat && lon > box.neCoordinates.lon => Coordinates(lat, lon)
+          case Coordinates(lat, _) if lat > box.neCoordinates.lat => Coordinates(lat, box.neCoordinates.lon)
+          case Coordinates(_, lon) if lon > box.neCoordinates.lon => Coordinates(box.neCoordinates.lat, lon)
+          case _ => box.neCoordinates
         }
-        BoundingBox(swCoordinate, neCoordinate)
+        BoundingBox(swCoordinates, neCoordinates)
       }
     )
   }
--- src/main/scala/com/shared/Models.scala
+++ src/main/scala/com/shared/Models.scala
@@ -5,8 +5,8 @@ import java.util.Date
 final case class URIs(uris: Vector[String])
 final case class ImageInfo(download: String, preview: String, cloud: Float, date: Date)
 final case class Coordinates(lat: Float, lon: Float)
-final case class BoundingBox(swCoordinate: Coordinates, neCoordinate: Coordinates) {
+final case class BoundingBox(swCoordinates: Coordinates, neCoordinates: Coordinates) {
   override def toString(): String = {
-    s"${swCoordinate.lat},${swCoordinate.lon},${neCoordinate.lat},${neCoordinate.lon}"
+    s"${swCoordinates.lat},${swCoordinates.lon},${neCoordinates.lat},${neCoordinates.lon}"
   }
 }
