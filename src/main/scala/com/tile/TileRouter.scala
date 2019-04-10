3e1bb708502fb71633877d817dd34bf44686c07e
Thomas <boulbestho@eisti.eu>
2019-04-10 09:57:15 +0200

    Created minimal routing for a file download POST /tile with body as [url1, url2, ...]

--- src/main/scala/com/tile/TileRouter.scala
+++ src/main/scala/com/tile/TileRouter.scala
@@ -6,17 +6,29 @@ import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.directives.RouteDirectives.complete
 import akka.util.Timeout
+import com.shared.JsonSupport
 import com.tile.TileRegistryActor.TileImage
+import spray.json.DefaultJsonProtocol
+
 import scala.concurrent.duration._
 
-class TileRouter(tileRegistryActor: ActorRef) {
+class TileRouter(tileRegistryActor: ActorRef) extends JsonSupport {
+
+  import DefaultJsonProtocol._
 
   implicit lazy val timeout: Timeout = Timeout(180.seconds)
 
-  lazy val route: Route = get {
-    path(Segment) { imageName =>
-       tileRegistryActor ! TileImage(imageName)
-      complete(StatusCodes.Accepted)
+  lazy val route: Route = {
+    get {
+      path(Segment) { imageName =>
+        tileRegistryActor ! TileImage(imageName)
+        complete(StatusCodes.Accepted)
+      }
+    }
+  } ~
+  post {
+    entity(as[Vector[String]]) { urls =>
+      complete(StatusCodes.OK, urls)
     }
   }
 }
